0c82e92a9d640f205b226ef0f270fe9cfa9e0060
RachidIferdin <sitrachid8@gmail.com>
2023-03-21 15:54:58 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/100-main.c
+++ 0x02-functions_nested_loops/100-main.c
@@ -1,3 +1,4 @@
+
 #include "main.h"
 
 /**
@@ -13,6 +14,6 @@ int main(void)
     _putchar('\n');
     print_times_table(98);
     _putchar('\n');
-    print_times_table(12);  
+    print_times_table(12);
     return (0);
 }
