9df0c4877e4228d833900307020f65d39ccabe91
RachidIferdin <sitrachid8@gmail.com>
2023-03-22 07:04:19 -0700

    tasks

--- 0x03-debugging/3-convert_day.c
+++ 0x03-debugging/3-convert_day.c
@@ -11,7 +11,7 @@
 int convert_day(int month, int day)
 {
 	switch (month)
-    {
+{
 	case 2:
 	day = 31 + day;
 	break;
@@ -47,6 +47,6 @@ int convert_day(int month, int day)
 	break;
 	default:
 	break;
-    }
+}
 	return (day);
 }
