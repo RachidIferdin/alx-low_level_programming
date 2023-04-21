de55b95374eafdb90d2463044d4863bc18a8d721
RachidIferdin <sitrachid8@gmail.com>
2023-04-20 17:46:38 -0700

    variadic_functions

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,28 +1,24 @@
+#include <stdarg.h>
 #include "variadic_functions.h"
 
 /**
- * sum_them_all - entry point
- * @n: size of triangle
- * Description: --
+ * sum_them_all - a function that returns the sum of all its parameters.
+ * @n: integers to add
  * Return: sum
  */
+
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list args;
-	int sum = 0;
-	int i = 0;
-
-	va_start(args, n);
+	va_list valist;
+	unsigned int sum = 0, i;
 
 	if (n == 0)
 		return (0);
+	va_start(valist, n);
 
-	while (i < (int)n)
-	{
-		sum += va_arg(args, int);
-		i++;
-	}
-	va_end(args);
+	for (i = 0; i < n; i++)
+		sum += va_arg(valist, int);
+	va_end(valist);
 
 	return (sum);
 }
